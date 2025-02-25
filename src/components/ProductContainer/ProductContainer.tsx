d5d5ec6543b181c52c93151bb36c741c88282d24
Luka <llesko00@fesb.hr>
2025-02-25 19:58:31 +0100

    Add products css

--- src/components/ProductContainer/ProductContainer.tsx
+++ src/components/ProductContainer/ProductContainer.tsx
@@ -1,12 +1,13 @@
 import { Product } from "../../types/Product";
+import "./ProductContainer.css";
 
 const ProductContainer: React.FC<{ product: Product }> = ({ product }) => {
     return (
-        <>
+        <div className="product-container">
             <h1>{product.title}</h1>
             <p>{product.price}</p>
             <img src={product.image} alt="product" />
-        </>
+        </div>
     );
 };
 
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,13 +1,14 @@
 import { Product } from "../../types/Product";
 import ProductContainer from "../ProductContainer/ProductContainer";
+import "./ProductList.css";
 
 const ProductList: React.FC<{ products: Product[] }> = ({ products }) => {
     return (
-        <>
+        <div className="product-list">
             {products.map((product) => (
                 <ProductContainer key={product.id} product={product} />
             ))}
-        </>
+        </div>
     );
 };
 
